95539b34423b432314cb521ef4f39f370e233584
Sam Chandler <samuelvanchandler@gmail.com>
2016-01-19 21:53:45 -0700

    pushed full content for milestone 1

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,18 +6,28 @@
 	</head>
 	<body>
 		<h1>Purpose</h1>
-		<p>The purpose of my personal website is to promote myself as an
-			entry-level jr developer, it should be a demonstartion of my
-			skill set. </p>
+		<p>The purpose of my personal website is to promote myself as an entry-level<br>
+			jr developer; it should be a demonstration of my skill set.
+		</p>
 		<h1>Audience</h1>
-		<p>The intended audience is employers and recruiters, directed
-			here from my linkedin profile.</p>
+		<p>The intended audience is employers and recruiters, directed here from my<br>
+			linkedin profile.
+		</p>
 		<h1>Goal</h1>
-		<p>The goal of this web page is to enhance my overall porfolio
-			and land me a job.</p>
+		<p>The goal of this web page is to enhance my overall porfolio and land me a job.
+		</p>
 		<h1>Persona</h1>
 		<p>Name: Sally Doolittle</p>
-		<p></p>
-
+		<p>Sally works in Human Resourses for a web development firm, or she is headhunter.<br>
+			She searched linkedin for keywords and found my profile, which direced her to this<br>
+			site. She is skeptical of someone with no professional experience, but my resume <br>
+			and porfolio are so badass, she gives me a call anyways.
+		</p>
+		<h1>Use Case</h1>
+		<p>Sally finds my web page during business hours, probably from an office desktop.<br>
+			She is looking for a cheap candidate for the position, that doesn't have much<br>
+			experience, but shows an enthusiasm for the technologies requisite and that<br>
+			presents her with the least amount of risk.
+			</p>
 	</body>
 </html>
