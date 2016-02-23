72e0b06683ffc2978a1046cd46d07cb8e15ebe4f
Sam Chandler <samuelvanchandler@gmail.com>
2016-02-23 14:52:15 -0700

    added nav bar template and media query css template

--- public_html/index.php
+++ public_html/index.php
@@ -27,6 +27,27 @@
 				<p>hey, it's a site....</p>
 				<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
 			</div>
+			<nav class="navbar navbar-inverse">
+				<!-- logo and mobile toggle button get grouped together for better mobile display -->
+				<div class="navbar-header">
+					<!-- this is the mobile menu button -->
+					<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu">
+						<span class="sr-only">main menu</span>
+						<span class="glyphicon glyphicon-menu-hamburger"></span>
+					</button>
+					<a class="navbar-brand" href="#">Your Logo/Text Here</a>
+				</div>
+
+				<!-- here are your main nav links, grouped for toggling -->
+				<div class="collapse navbar-collapse" id="main-menu">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Link1</a></li>
+						<li><a href="#">Link2</a></li>
+						<li><a href="#">Link3</a></li>
+					</ul>
+				</div>
+			</nav>
 		</div>
 		<img src="documentation/image/wireframes.gif" alt="wireframes" />
 	</body>
