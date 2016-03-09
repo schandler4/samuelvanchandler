6064064e4339ee80b2efb0b17e4e2b4d4509c92e
Sam Chandler <samuelvanchandler@gmail.com>
2016-03-09 13:01:00 -0700

    changes to navbar and fonts

--- public_html/index.php
+++ public_html/index.php
@@ -6,67 +6,74 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-		<link type="text/css" href="css/styles" rel="stylesheet">
-		<!--Optional theme-->
-<!--		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+				integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link type="text/css" href="css/styles.css" rel="stylesheet">
+		<!--		Optional theme-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+				integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
 
-		<!-- Latest compiled and minified JavaScript -->
-		<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>-->
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<!--		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>-->
-		<!--Latest compiled and minified Bootstrap JavaScript, all compiled plugins included-->
-<!--		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>-->
+		<!--		 Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+				  crossorigin="anonymous"></script>
+		<!--		 jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<!--		Latest compiled and minified Bootstrap JavaScript, all compiled plugins included-->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+				  crossorigin="anonymous"></script>
 
 		<title>samuelvanchandler</title>
 	</head>
-	<div class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Brand</a>
+	<header id="top">
+		<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#top">Top</a></li>
+						<li><a href="#works">Works</a></li>
+						<li><a href="#contact">Contact</a></li>
+					</ul>
+				</div><!--/.nav-collapse -->
 			</div>
-			<div class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li class="active"><a href="#">Home</a></li>
-					<li><a href="#about">About</a></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#">Username</a></li>
-				</ul>
-			</div><!--/.nav-collapse -->
 		</div>
-	</div>
-
+	</header>
 	<div class="wide">
-		<div class="col-xs-5 line"><hr></div>
-		<div class="col-xs-2 logo">Logo</div>
-		<div class="col-xs-5 line"><hr></div>
+		<div class="col-xs-5"></div>
+		<div class="col-xs-2">
+			<h1>Samuel Van Chandler</h1>
+		</div>
+		<div class="col-xs-5"></div>
 	</div>
 	<section>
-		<div class="container-fluid">
+		<div id="works" class="container-fluid">
 			<div class="row">
-				<div id="works" class="col-xs-12">
+				<div class="col-xs-12">
 					<h2>Works</h2>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-4">
-					<a href="img/panda-sleep-2.jpg" class="pic">
+					<a href="img/panda-sleep-2.jpg" class="thumbnail">
 						<img src="img/panda-sleep-2.jpg" alt="panda">
 					</a>
 				</div>
 				<div class="col-md-4">
-					<a href="img/Panda_Cub.JPG" class="pic">
+					<a href="img/Panda_Cub.JPG" class="thumbnail">
 						<img src="img/Panda_Cub.JPG" alt="panda">
 					</a>
 				</div>
 				<div class="col-md-4">
-					<a href="img/t1largpanda.jpg" class="pic">
+					<a href="img/t1largpanda.jpg" class="thumbnail">
 						<img src="img/t1largpanda.jpg" alt="panda">
 					</a>
 				</div>
@@ -75,7 +82,7 @@
 				<div class="col-md-2"></div>
 				<div class="col-md-4">
 
-					<a href="img/t1largpanda.jpg"  class="thumbnail">
+					<a href="img/t1largpanda.jpg" class="thumbnail">
 						<img src="img/t1largpanda.jpg" alt="panda">
 					</a>
 				</div>
@@ -103,6 +110,7 @@
 			</div>
 		</div>
 	</footer>
+</html>
 <!--	<div class="container">-->
 <!--		<div class="text-center">-->
 <!--			<h1>Content</h1>-->
